46df885632a8e3221fb5d36b339a89e95561781a
Bima Pangestu <bimapangestu280@gmail.com>
2019-03-08 07:17:34 +0700

    middleware jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserDTO, UserRO } from './user.dto';
+import { AuthGuard } from '../shared/auth.guard';
+import { User } from './user.decorator';
 
 @Controller()
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Get('api/users')
-  async showAllUsers() {
+  @UseGuards(new AuthGuard())
+  async showAllUsers(@User() user) {
     return await this.userService.showAll();
   }
 
